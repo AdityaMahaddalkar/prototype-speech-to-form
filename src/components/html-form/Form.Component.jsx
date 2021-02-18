7af83da4b9c1de81ccb9cf6bbc32416c1f54e579
Aditya Mahaddalkar <adityam1311@gmail.com>
2021-02-18 16:35:39 +0530

    Added select command in the form component for selecting by id

--- src/components/html-form/Form.Component.jsx
+++ src/components/html-form/Form.Component.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 function FormComponent() {
+  const [selectedId, setSelectedId] = React.useState(null);
+  const [inputValue, setInputValue] = React.useState(null);
+  const commands = [
+    {
+      command: "select :condition",
+      callback: (condition) => {
+        console.log(`Select ${condition}`);
+        try {
+          document.getElementById(`${condition}`).style.backgroundColor =
+            "#fad698";
+        } catch (e) {
+          console.error(e);
+        }
+
+        for (let id of ids) {
+          console.log(`${id}\t${condition}`);
+          if (id !== condition && document.getElementById(id)) {
+            console.log(`Resetting background of ${id}`);
+            document.getElementById(`${id}`).style.backgroundColor =
+              "transparent";
+          }
+        }
+      },
+    },
+  ];
+
+  const ids = ["email", "password", "address", "phone"];
+
+  const transcripts = useSpeechRecognition({ commands });
+
+  SpeechRecognition.startListening({ continuous: true });
+
   return (
     <Container className="mt-5">
       <div>Welcome to forms</div>
@@ -9,9 +44,10 @@ function FormComponent() {
         <Form>
           <Row>
             <Col>
-              <Form.Group controlId="customEmail">
+              <Form.Group>
                 <Form.Label>Email</Form.Label>
                 <Form.Control
+                  id={ids[0]}
                   type="email"
                   placeholder="Enter email"
                   autoComplete={false}
@@ -19,9 +55,10 @@ function FormComponent() {
               </Form.Group>
             </Col>
             <Col>
-              <Form.Group controlId="customPassword">
+              <Form.Group>
                 <Form.Label>Password</Form.Label>
                 <Form.Control
+                  id={ids[1]}
                   type="password"
                   placeholder="Enter password"
                   autoComplete={false}
@@ -31,9 +68,10 @@ function FormComponent() {
           </Row>
           <Row>
             <Col>
-              <Form.Group controlId="customAddress">
+              <Form.Group>
                 <Form.Label>Address</Form.Label>
                 <Form.Control
+                  id={ids[2]}
                   as="textarea"
                   rows={3}
                   autoComplete={false}
@@ -41,9 +79,9 @@ function FormComponent() {
               </Form.Group>
             </Col>
             <Col>
-              <Form.Group controlId="customPhone">
-                <Form.Label>Phone Number</Form.Label>
-                <Form.Control type="number"></Form.Control>
+              <Form.Group>
+                <Form.Label>Phone</Form.Label>
+                <Form.Control type="number" id={ids[3]}></Form.Control>
               </Form.Group>
             </Col>
           </Row>
